510cc66c5f5852e9a97853a5b7af7e47a0b0e911
Tomo Masakura <masakura@codearts.co.jp>
2019-07-01 17:33:40 +0900

    プロジェクト ファイルを追加します。

---
+++ AzureLogApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureLogApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
